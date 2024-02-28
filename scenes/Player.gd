20339599c8553141dd141de45abec9843f9d174c
alvaro.luqman1 <alvaro.luqman@gmail.com>
2024-02-28 20:54:22 +0700

    Finished

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -12,16 +12,20 @@ var crouch_texture : Texture = load("res://assets/kenney_platformercharacters/PN
 var idle_texture : Texture = load("res://assets/kenney_platformercharacters/PNG/Adventurer/Poses/adventurer_idle.png")
 var jump_texture: Texture = load("res://assets/kenney_platformercharacters/PNG/Adventurer/Poses/adventurer_jump.png")
 var walk_texture: Texture = load("res://assets/kenney_platformercharacters/PNG/Adventurer/Poses/adventurer_walk1.png")
+var dash_texture: Texture = load("res://assets/kenney_platformercharacters/PNG/Adventurer/Poses/adventurer_slide.png")
 
+var lastTapTime = 0.0
+var doubleTapInterval = 0.5
 var velocity = Vector2()
 var jumps_remaining = 0
+var double_tap_count = 0
 
 var crouching = false
+var dashing = false
 
-func get_input():
+func get_input(sprite):
 	velocity.x = 0
-	var sprite = get_node("Sprite")
-
+	
 	if is_on_floor():
 		jumps_remaining = max_jumps
 
@@ -33,6 +37,23 @@ func get_input():
 	if Input.is_action_just_released('ui_up'):
 		sprite.texture = idle_texture
 
+	if Input.is_action_just_pressed('ui_right'):
+		var currentTime = OS.get_ticks_msec()/1000.0
+		if currentTime - lastTapTime < doubleTapInterval:
+			sprite.texture = dash_texture
+			velocity.x += speed*10
+
+		lastTapTime = currentTime
+	
+	if Input.is_action_just_pressed('ui_left'):
+		var currentTime = OS.get_ticks_msec()/1000.0
+		if currentTime - lastTapTime < doubleTapInterval:
+			sprite.texture = dash_texture
+			velocity.x -= speed*10
+			
+		lastTapTime = currentTime
+
+					
 	if Input.is_action_pressed('ui_right'):
 		sprite.flip_h = false
 		if crouching:
@@ -53,14 +74,14 @@ func get_input():
 		if crouching:
 			sprite.texture = crouch_texture
 		else:
-			sprite.texture = walk_texture
+			sprite.texture = idle_texture
 		velocity.x += speed
 		
 	if Input.is_action_just_released('ui_left'):
 		if crouching:
 			sprite.texture = crouch_texture
 		else:
-			sprite.texture = walk_texture
+			sprite.texture = idle_texture
 		velocity.x -= speed
 		
 	if Input.is_action_just_pressed("ui_down"):
@@ -72,10 +93,14 @@ func get_input():
 		crouching = false
 		speed = 400
 		sprite.texture = idle_texture
+		
+
 
 func _physics_process(delta):
+	var sprite = get_node("Sprite")
+	
 	velocity.y += delta * GRAVITY
-	get_input()
+	get_input(sprite)
 	velocity = move_and_slide(velocity, UP)
 
 
